91e834eab74c6347210e6755a0ab57399c0a2153
gammafp <gammafp@gmail.com>
2019-01-08 18:42:31 +0100

    Generar posiciones

--- 8_puzzle/src/IA/IA.js
+++ 8_puzzle/src/IA/IA.js
@@ -1,7 +1,6 @@
 const R = require('ramda');
 const helpers = require('./helpers.js');
 
-
 const tablero = [
     [8, 1, 2],
     [3, 4, 5],
@@ -42,6 +41,40 @@ heuristicTomato(tablero)
 
 
 // Inicio motor IA
-for (let i = 0; i < 4; i++) {
+let tableroTemp = [];
+let listaPeso = [];
 
+// Variables de uso para obtener el indice y la colisión.
+const indexVacio = R.flatten(tablero).indexOf(0); 
+const indexBiVacio = helpers.findBi(indexVacio);
+const tableroFlat = R.flatten(tablero);
+const collision = helpers.checkCollitionIA(indexVacio, tableroFlat);
+
+// Generamos los posibles movimientos (hay que pensar como hacer para movimientos hijos)
+if(!collision.up) {
+    tableroTemp = R.clone(tablero);
+    tableroTemp[indexBiVacio.y][indexBiVacio.x] = tableroTemp[indexBiVacio.y-1][indexBiVacio.x];
+    tableroTemp[indexBiVacio.y-1][indexBiVacio.x] = 0;
+    listaPeso.push({value: heuristicTomato(tableroTemp), mov: 'up'});
 }
+if(!collision.down) {
+    tableroTemp = R.clone(tablero);
+    tableroTemp[indexBiVacio.y][indexBiVacio.x] = tableroTemp[indexBiVacio.y+1][indexBiVacio.x];
+    tableroTemp[indexBiVacio.y+1][indexBiVacio.x] = 0;
+    listaPeso.push({ value: heuristicTomato(tableroTemp), mov: 'down'});
+}
+if(!collision.left) {
+    tableroTemp = R.clone(tablero);
+    tableroTemp[indexBiVacio.y][indexBiVacio.x] = tableroTemp[indexBiVacio.y][indexBiVacio.x-1];
+    tableroTemp[indexBiVacio.y][indexBiVacio.x-1] = 0;
+    listaPeso.push({value: heuristicTomato(tableroTemp), mov: 'left'});
+}
+if(!collision.right) {
+    tableroTemp = R.clone(tablero);
+    tableroTemp[indexBiVacio.y][indexBiVacio.x] = tableroTemp[indexBiVacio.y-1][indexBiVacio.x+1];
+    tableroTemp[indexBiVacio.y][indexBiVacio.x+1] = 0;
+    listaPeso.push({value: heuristicTomato(tableroTemp), mov: 'right'});
+}
+
+// 
+console.log(listaPeso);
